2d192be3a45f7e6bee370c064fa1ab81e88363f4
locnqt <locnqt@gmail.com>
2018-04-03 10:04:05 +0700

    fix encode

--- LSB/src/lsb/LSB.java
+++ LSB/src/lsb/LSB.java
@@ -17,8 +17,9 @@ import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 
 /**
- *
- * @author locnqt LSB 24bit
+ *  
+ * @author Nguyen Quang Tuan Loc _ N14DCAT097
+ * LSB 24bit
  */
 public class LSB {
 
@@ -34,7 +35,7 @@ public class LSB {
         while (n != 3) {
             try {
                 System.out.println("---------------------------------------------------------------");
-                System.out.print(" 1.Encode \n 2.Decode \n 3.Exit \n Chọn 1-3:");
+                System.out.print(" 1.Encode \n 2.Decode \n 3.Exit \n choose(1-3):");
                 n = sc.nextInt();
                 switch (n) {
                     case 1:
@@ -46,10 +47,10 @@ public class LSB {
                     case 3:
                         break;
                     default:
-                        System.out.println("Nhập sai!");
+                        System.out.println("ERROR! You must choose 1->3!");
                 }
             } catch (java.util.InputMismatchException ioe) {
-                System.err.println("!!Yêu cầu nhập số!!");
+                System.err.println("!!ERROR! You must choose 1->3!!");
                 break;
             }
         }
@@ -58,11 +59,8 @@ public class LSB {
     public static String messtobin(String mess) {
         String result = "";
         for (char c : mess.toCharArray()) {
-//            System.out.println(c);
-//            System.out.println(Integer.toBinaryString(Character.getNumericValue(c))); 
-//            System.out.println(Character.getNumericValue(c));
             String bin = Integer.toBinaryString(c);
-            while (bin.length() < 8) {
+            while (bin.length() < 8) { //them 0 vao dau cho du 8 bit
                 bin = "0" + bin;
             }
             result += bin;
@@ -70,101 +68,73 @@ public class LSB {
         return result;
     }
 
-    public static char bintomess(String bin) {
-        byte r = 0;
-        int n = bin.length();
-        int i = 0;
-        int pow = 1;
-        for (char c : bin.toCharArray()) {
-            if (c == '1') {
-                i = 0;
-                pow = 1;
-                while (i < n - 1) {
-                    pow *= 2;
-                    i++;
-                }
-                r += pow;
-            }
-            n--;
-        }
-        return (char) r;
+    public static String bintomess(String bin) {
+        int charCode = Integer.parseInt(bin, 2);
+        System.out.println(charCode);
+        String result = new Character((char)charCode).toString();
+        return result;
     }
 
     public static void Encode() throws IOException {
-        System.out.print("Nhập message: ");
+        System.out.print("Enter you message: ");
         sc.nextLine();
         String mess = sc.nextLine();
-        String bin = messtobin(mess)+"00000000";
-        char[] b = bin.toCharArray();
-//        String databin = "";
-//        System.out.println(bin);
-
-        File f = new File("sample-pic.bmp");
+        String bin = messtobin(mess)+"00000000"; //them null vao cuoi message de danh dau ket thuc
+        
+        System.out.print("Enter bitmap file name: ");
+        String srcname = sc.nextLine();
+        
+        File f = new File(srcname+".bmp");
         BufferedImage bufferedImage = ImageIO.read(f);
         
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         ImageIO.write(bufferedImage, "bmp", bos);
         byte[] arr = bos.toByteArray();
-//        StringBuilder sb = new StringBuilder();
-//        for (byte by : arr) {
-//            sb.append(Integer.toBinaryString(by & 0xFF));
-//        }
-//        System.out.println(sb);
-//        can chuyen thanh binary
-        int data = HEADER_SIZE+1;
+        
+        int data = HEADER_SIZE;
         char temp;
-//        String newdata = "";
-//        String newdataimage = "";
-//        String header = "";
         int x = 0;
-        if (bin.length() > (arr.length - 1)) {
-            System.out.println("message quá lớn!");
+        if (bin.length() > (arr.length - 54)) {
+            System.out.println("message to large for image to hide!");
         } else {
-            for (int i = data; x < bin.length(); i++) {
-//                temp = bin.charAt(x);
-                
-                arr[i] >>= 1;
-                arr[i] <<= 1;
-                arr[i] += b[x];x++;
-//                databin = Integer.toBinaryString((arr[i] + 256) % 256);
-//                temp = bin.charAt(x);
-//                x++;
-//                newdata = databin.substring(0, 7) + temp;
-//                newdataimage += newdata;
-//                arr[i] = newdata.getBytes();
-//                System.out.println(databin);
-//                System.out.println(temp);
-//                System.out.println(newdata);
-//                System.out.println(newdataimage);
-//                System.out.println(arr[i]);
+            for (int i = 0; i < bin.length(); i++) {
+                arr[data] >>= 1;
+                arr[data] <<= 1;
+                arr[data] += (bin.charAt(i)-48);   //
+                data++;
             }
         }
-        File output = new File("encrypted.bmp");
+        
+        System.out.print("Enter name file you want to save encrypted image: ");
+        String destname = sc.nextLine();
+        
+        File output = new File(destname+".bmp");
         bufferedImage = ImageIO.read(new ByteArrayInputStream(arr));
-//        ImageIO.write(bufferedImage, "bmp",new File("encrypted.bmp" + ".bmp"));
         if (ImageIO.write(bufferedImage, "bmp",output)) {
             System.out.println("Save file OK!");
         } else {
-            System.out.println("ERROR!! Không save duoc!!");
+            System.out.println("ERROR!! Cannot save!!");
         }
     }
 
     public static void Decode() throws IOException {
-        File f = new File("encrypted.bmp");
+        
+        sc.nextLine();
+        System.out.print("Enter bitmap file you want to decrypt: ");
+        String destname = sc.nextLine();
+        
+        File f = new File(destname+".bmp");
         BufferedImage bufferedImage = ImageIO.read(f);
-        FileInputStream fis = new FileInputStream(f);
-        byte[] buf = new byte[1024];
+        
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        for (int readNum; (readNum = fis.read(buf)) != -1;) {
-            bos.write(buf, 0, readNum);
-        }
+        ImageIO.write(bufferedImage, "bmp", bos);
         byte[] arr = bos.toByteArray();
 
-        int data = HEADER_SIZE + 1;
+        int data = HEADER_SIZE;
         String databin = "";
         String mess = "";
-        int ktra8bit0 = 0;
-        int count = 0;
+        int ktra8bit0 = 0; // kiem tra 8 bit 0 da duoc danh dau cuoi message
+        int count = 0; // dem bit, du 8 bit thanh 1 byte --> ktra8bit=0;
         while (true) {
             if (arr[data] % 2 == 0) {
                 databin += "0";
@@ -184,13 +154,10 @@ public class LSB {
                 count = 0;
             }
         }
-        databin = databin.substring(0, databin.length() - 8);
-//        System.out.println(databin);
+        databin = databin.substring(0, databin.length() - 8); // cat 8 bit 0 cuoi message
         for (int i = 0; i < databin.length(); i = i + 8) {
             mess += bintomess(databin.substring(i, i + 8));
         }
-//        int charCode = Integer.parseInt(databin, 2);
-//        String mess = new Character((char) charCode).toString();
         System.out.println("Message: " + mess);
     }
 }
